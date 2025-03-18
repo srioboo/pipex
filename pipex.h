aedfde4eee95504e7db95809bc1291e180ca5a26
Salvador Rioboo Naranjo <srioboo-@student.42malaga.com>
2025-03-18 10:24:04 +0100

    fix: correct norminette

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: srioboo- <srioboo-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 09:54:19 by srioboo-          #+#    #+#             */
-/*   Updated: 2025/03/18 10:08:08 by srioboo-         ###   ########.fr       */
+/*   Updated: 2025/03/18 10:22:54 by srioboo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@
 
 // TODO
 # include <stdio.h>
+
+void	dummy_function(void);
 
 #endif
