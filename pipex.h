b85ac249e45508131bf38d8efd3ab04de2fe11a9
srioboo <salrio74@gmail.com>
2025-04-23 22:53:50 +0200

    feat: add function for loading files

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: srioboo- <srioboo-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 09:54:19 by srioboo-          #+#    #+#             */
-/*   Updated: 2025/03/18 12:10:32 by srioboo-         ###   ########.fr       */
+/*   Updated: 2025/04/23 22:52:23 by srioboo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
 # include "libft/src/libft.h"
 # include "libft/ft_printf/ft_printf.h"
 
 void	dummy_function(void);
+int		load_file(char *file_path);
+void	close_file(int fd);
 
 #endif
