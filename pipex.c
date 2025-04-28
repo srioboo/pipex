62cd04a61d37d296497d27948ca6b017a65edaae
Salvador Rioboo Naranjo <srioboo-@student.42malaga.com>
2025-04-28 11:46:21 +0200

    feat: add pipe example

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: srioboo- <srioboo-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 10:23:06 by srioboo-          #+#    #+#             */
-/*   Updated: 2025/04/28 11:05:19 by srioboo-         ###   ########.fr       */
+/*   Updated: 2025/04/28 11:51:53 by srioboo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,48 @@
 	ft_printf("\t test: %s exist: %d\n", path2, access(path2, R_OK | W_OK | X_OK));
 	return (access(path1, mode1));
 }*/
+
+int	tuberia()
+{
+	int	pipefd[2];
+	int pid;
+	char buffer[100];
+
+	if(pipe(pipefd) == -1)
+	{
+		perror("An error in pipe as happend");
+		exit(EXIT_FAILURE);
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error creating child proccess");
+		exit(EXIT_FAILURE);
+	}
+
+	if (pid == 0) { // Proceso hijo
+		close(pipefd[1]); // Cerrar el extremo de escritura
+
+		// Leer del pipe
+		read(pipefd[0], buffer, sizeof(buffer));
+		printf("Hijo recibió: %s\n", buffer);
+
+		close(pipefd[0]); // Cerrar el extremo de lectura
+		exit(EXIT_SUCCESS);
+	} else { // Proceso padre
+		close(pipefd[0]); // Cerrar el extremo de lectura
+
+		// Escribir en el pipe
+		const char *message = "Hola desde el proceso padre!";
+		write(pipefd[1], message, strlen(message) + 1);
+
+		close(pipefd[1]); // Cerrar el extremo de escritura
+		wait(NULL); // Esperar a que el hijo termine
+	}
+
+	return 0;
+}
 
 void	pipex(int argc, char **argv)
 {
@@ -35,7 +77,8 @@ void	pipex(int argc, char **argv)
 		if (param == 2)
 		{
 			ft_printf("Param 2: %s\n", argv[param]);
-			ft_split(param, ' ');
+			char **command1 = ft_split(argv[param], ' ');
+			ft_printf("comand 1 splitted: %s %s\n", command1[0], command1[1]);
 		}
 		if (param == 3)
 			ft_printf("Param 3: %s\n", argv[param]);
@@ -51,7 +94,7 @@ void	pipex(int argc, char **argv)
 			
 			// TODO - example 
 			char *filename = "/usr/bin/ls";
-    		char *arguments[] = {"/usr/bin/ls", "-l", NULL};
+			char *arguments[] = {"/usr/bin/ls", "-l", NULL};
 			char *env[] = { NULL };
 			if (execve(filename, arguments, env) == -1)
 				ft_printf("se produce un error");
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: srioboo- <srioboo-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 09:54:19 by srioboo-          #+#    #+#             */
-/*   Updated: 2025/04/23 22:52:23 by srioboo-         ###   ########.fr       */
+/*   Updated: 2025/04/28 11:51:22 by srioboo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/wait.h>
+
 # include "libft/src/libft.h"
 # include "libft/ft_printf/ft_printf.h"
 
