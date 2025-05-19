82c72383f8213b8d46084804207692fff09545fa
srioboo <salrio74@gmail.com>
2025-05-19 09:16:01 +0200

    feat: test.sh changes

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ clear && make test
 # ./pipex infile "cat" "cat" outfile
 # printf "\nConvertir todas las letras \"a\" en \"o\"\n"
 # ./pipex infile "tr 'a' 'o'" "cat" outfile
-# printf "\nPasar todo el texto a mayúsculas\n"
-# ./pipex infile "tr 'a-z' 'A-Z'" "cat" outfile
+printf "\nPasar todo el texto a mayúsculas\n"
+ ./pipex infile "tr 'a-z' 'A-Z'" "cat" outfile
 printf "\nContar el número de palabras\n"
 ./pipex infile "cat" "wc -w" outfile
 # printf "\nOrdenar alfabéticamente el contenido del archivo\n"
