c6f285ab82092c9cf2190fc4e40b6bc9551ad935
Salvador Rioboo Naranjo <srioboo-@student.42malaga.com>
2025-05-23 10:44:27 +0200

    feat: test file improve

--- test.sh
+++ test.sh
@@ -2,16 +2,70 @@
 
 clear && make re
 
-# printf "\nCopiar el contenido de infile en outfile\n"
-# ./pipex infile "cat" "cat" outfile
-printf "\nConvertir todas las letras \"a\" en \"o\"\n"
-./pipex infile "tr 'a' 'o'" "cat" outfile
-# printf "\nPasar todo el texto a mayúsculas\n"
-# ./pipex infile "tr 'a-z' 'A-Z'" "cat" outfile
-#printf "\nContar el número de palabras\n"
-#./pipex infile "cat" "wc -w" outfile
-# printf "\nOrdenar alfabéticamente el contenido del archivo\n"
-# ./pipex infile "sort" "cat" outfile
+# EXEC="valgrind --leak-check=summary ./pipex"
+EXEC="./pipex" 
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+NAVY='\033[0;34m'
+BLUE='\033[1;34m'
+AQUA='\033[1;36m'
+MAGENTA='\033[1;35m'
+GRAY='\033[1;30m'
+WHITE='\033[1;37m'
+NC="\033[m"
 
-# % ./pipex  infile"" "     " out.txt
-# % ./pipex infile "ls" "" out.txt
+function test1()
+{
+	printf "\n${GREEN}=== TEST: ${NC}Copy infile content into outfile\n"
+	${EXEC} infile "cat" "cat" $1
+	printf "${YELLOW}=== Result file: ${NC}$1\n\n"
+	cat $1
+	printf "\n${GREEN}=== END TEST${NC}\n\n"
+}
+
+function test2()
+{
+	printf "\n${GREEN}=== TEST: ${NC}Convert all \"a\" letter into \"o\"\n"
+	${EXEC} infile "tr 'a' 'o'" "cat" $1
+	printf "${YELLOW}=== Result file: ${NC}$1\n\n"
+	cat $1
+	printf "\n${GREEN}=== END TEST${NC}\n\n"
+}
+
+function test3()
+{
+	printf "\n${GREEN}=== TEST: ${NC}Convert to upper\n"
+	${EXEC} infile "tr 'a-z' 'A-Z'" "cat" $1
+	printf "${YELLOW}=== Result file: ${NC}$1\n\n"
+	cat $1
+	printf "\n${GREEN}=== END TEST${NC}\n\n"
+}
+
+function test4()
+{
+	printf "\n${GREEN}=== TEST: ${NC}Count words\n"
+	${EXEC} infile "cat" "wc -w" $1
+	printf "${YELLOW}=== Result file: ${NC}$1\n\n"
+	cat $1
+	printf "\n${GREEN}=== END TEST${NC}\n\n"
+}
+
+function test5()
+{
+	printf "\n${GREEN}=== TEST: ${NC}Order alphabetical the file content\n"
+	${EXEC} infile "sort" "cat" $1
+	printf "${YELLOW}=== Result file: ${NC}$1\n\n"
+	cat $1
+	printf "\n${GREEN}=== END TEST${NC}\n\n"
+}
+
+# % ${EXEC}  infile"" "     " out.txt
+# % ${EXEC} infile "ls" "" out.txt
+
+test1 outfile-cat.txt
+test2 outfile-convert-letters.txt
+test3 outfile-to-upper.txt
+test4 outfile-count-words.txt
+test5 outfile-order-alphabetical.txt
+# test6
