99a8a9a012ac3e5cedf3ccb4605a2b10c759b0b4
srioboo <salrio74@gmail.com>
2025-05-24 18:59:47 +0200

    refactor: change method process_parameters to void

--- pipex.c
+++ pipex.c
@@ -6,13 +6,13 @@
 /*   By: srioboo- <srioboo-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 10:23:06 by srioboo-          #+#    #+#             */
-/*   Updated: 2025/05/18 20:24:18 by srioboo-         ###   ########.fr       */
+/*   Updated: 2025/05/24 18:59:03 by srioboo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-t_pipex_data	*process_parameters(t_pipex_data *pipex_data,
+static void	process_parameters(t_pipex_data **pipex_data,
 					int argc, char **argv)
 {
 	int	param;
@@ -21,16 +21,15 @@ t_pipex_data	*process_parameters(t_pipex_data *pipex_data,
 	while (param <= (argc - 1))
 	{
 		if (param == 1)
-			pipex_data->infile = argv[param];
+			(*pipex_data)->infile = argv[param];
 		if (param == 2)
-			pipex_data->program_a = argv[param];
+			(*pipex_data)->program_a = argv[param];
 		if (param == 3)
-			pipex_data->program_b = argv[param];
+			(*pipex_data)->program_b = argv[param];
 		if (param == 4)
-			pipex_data->outfile = argv[param];
+			(*pipex_data)->outfile = argv[param];
 		param++;
 	}
-	return (pipex_data);
 }
 
 void	pipex(int argc, char **argv, char **envp)
@@ -40,7 +39,7 @@ void	pipex(int argc, char **argv, char **envp)
 	pipex_data = (t_pipex_data *)malloc(sizeof(t_pipex_data));
 	if (!pipex_data)
 		pipex_data = NULL;
-	pipex_data = process_parameters(pipex_data, argc, argv);
+	process_parameters(&pipex_data, argc, argv);
 	pipex_process(pipex_data, envp);
 }
 
