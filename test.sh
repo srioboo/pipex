8b7d13cd7b052f0e42f62bbc305d073a234e6701
Salvador Rioboo Naranjo <srioboo-@student.42malaga.com>
2025-07-10 18:09:23 +0200

    feat: update test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 clear
 make fclean
 make ${1}
+clear
 
 # EXEC="valgrind --leak-check=summary ./pipex"
 EXEC="./pipex" 
@@ -27,61 +28,19 @@ function show_result()
 	fi
 }
 
-function test1()
+function test_base()
 {
-	printf "\n${BLUE}=== TEST 0.1: ${NC}Copy infile content into outfile\n"
-	${EXEC} infile "cat" "cat" ${1}
-	cat < infile | cat > ${1}-orig
-	printf "${YELLOW}=== Result file: ${NC}${1}\n\n"
-	diff -u ${1} ${1}-orig
-	status=$?
-	show_result $status
-	printf "\n${BLUE}=== END TEST${NC}\n\n"
-}
-
-function test2()
-{
-	printf "\n${BLUE}=== TEST 0.2: ${NC}Convert all \"a\" letter into \"o\"\n"
-	${EXEC} infile "tr 'a' 'o'" "cat" ${1}
-	tr 'a' 'o' < infile | cat > ${1}-orig
-	printf "${YELLOW}=== Result file: ${NC}${1}\n\n"
-	diff -u ${1} ${1}-orig
-	status=$?
-	show_result $status
-	printf "\n${BLUE}=== END TEST${NC}\n\n"
-}
-
-function test3()
-{
-	printf "\n${BLUE}=== TEST 0.3: ${NC}Convert to upper\n"
-	${EXEC} infile "tr 'a-z' 'A-Z'" "cat" ${1}
-	tr 'a-z' 'A-Z' < infile | cat > ${1}-orig
-	printf "${YELLOW}=== Result file: ${NC}${1}\n\n"
-	diff -u ${1} ${1}-orig
-	status=$?
-	show_result $status
-	printf "\n${BLUE}=== END TEST${NC}\n\n"
-}
-
-function test4()
-{
-	printf "\n${BLUE}=== TEST 0.4: ${NC}Count words\n"
-	${EXEC} infile "cat" "wc -w" ${1}
-	cat < infile | wc -w > ${1}-orig
-	printf "${YELLOW}=== Result file: ${NC}${1}\n\n"
-	diff -u ${1} ${1}-orig
-	status=$?
-	show_result $status
-	printf "\n${BLUE}=== END TEST${NC}\n\n"
-}
-
-function test5()
-{
-	printf "\n${BLUE}=== TEST 0.5: ${NC}Order alphabetical the file content\n"
-	${EXEC} infile "sort" "cat" ${1}
-	sort < infile | cat > ${1}-orig
-	printf "${YELLOW}=== Result file: ${NC}${1}\n\n"
-	diff -u ${1} ${1}-orig
+	local test_num=$1
+	local test_desc=$2
+	local cmd1=$3
+	local cmd2=$4
+	local outfile=$5
+	
+	printf "\n${BLUE}=== TEST ${test_num}: ${NC}${test_desc}\n"
+	${EXEC} infile "${cmd1}" "${cmd2}" ${outfile}
+	${cmd1} < infile | ${cmd2} > ${outfile}-orig
+	printf "${YELLOW}=== Result file: ${NC}${outfile}\n\n"
+	diff -u ${outfile} ${outfile}-orig
 	status=$?
 	show_result $status
 	printf "\n${BLUE}=== END TEST${NC}\n\n"
@@ -137,11 +96,13 @@ function test-error5()
 	printf "\n${BLUE}=== END TEST${NC}\n\n"
 }
 
-test1 outfile-cat
-test2 outfile-convert-letters
-test3 outfile-to-upper
-test4 outfile-count-words
-test5 outfile-order-alphabetical
+test_base "0.1" "Copy infile content into outfile" "cat" "cat" outfile-cat
+test_base "0.2" "Convert all \"a\" letter into \"o\"" "tr 'a' 'o'" "cat" outfile-to-upper
+test_base "0.3" "Convert to upper" "tr 'a-z' 'A-Z'" "cat" outfile-to-upper
+test_base "0.4" "Count words" "cat" "wc -w" outfile-count-words
+test_base "0.5" "Order alphabetical the file content" "sort" "cat" outfile-order-alphabetical
+
+
 test-error1 outfile-error1
 test-error2 outfile-error2
 test-error3 outfile-error3
